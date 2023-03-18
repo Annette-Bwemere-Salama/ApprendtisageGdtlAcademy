6b960f1019a3883ef54cc028201b22c1ee062a76
Annette Bwemere <bwemereannette@gmail.com>
2023-03-18 14:17:07 +0200

    sizeof

--- data_types.c
+++ data_types.c
@@ -3,7 +3,16 @@
 #include <limits.h>
 #include <float.h>
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
     printf("CHAR_BIT : %d\n", CHAR_BIT);
+    printf("CHAR_MAX : %d\n", CHAR_MAX);
+    printf("CHAR_MIN : %d\n", CHAR_MIN);
+    printf("INT_MAX : %d\n",(long) INT_MAX);
+    printf("INT_MIN : %d\n",(long) INT_MIN);
+    printf("LONG_MIN : %d\n", LONG_MIN);
+    printf("LONG_MAX : %d\n", LONG_MAX);
+    printf("CHAR_MAX : %d\n", CHAR_MAX);
+    printf("CHAR_MAX : %d\n", CHAR_MAX);
 }
