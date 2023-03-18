9bfb5bfd7e36aa2aa1b1d0d1898ef4bbdeb2a93f
Annette Bwemere <bwemereannette@gmail.com>
2023-03-18 14:27:25 +0200

    finish Dataé

--- data_types.c
+++ data_types.c
@@ -15,4 +15,13 @@ int main(int argc, char **argv)
     printf("LONG_MAX : %d\n", LONG_MAX);
     printf("SCHAR_MAX : %d\n", SCHAR_MAX);
     printf("SCHAR_MIN : %d\n", SCHAR_MIN);
+    printf("SHRT_MAX : *d\n", SHRT_MAX);
+    printf("SHRT_MIN : *d\n", SHRT_MIN);
+    printf("UCHAR_MAX : *d\n", UCHAR_MAX);
+    printf("UINT_MAX : *d\n",(unsigned int) UINT_MAX);
+    printf("ULONG_MAX : *d\n",(unsigned long) ULONG_MAX);
+    printf("USHRT_MAX : *d\n",(unsigned short) USHRT_MAX);
+
+    return 0;
+
 }
